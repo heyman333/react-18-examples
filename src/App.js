27e5155a27a7a5b2177f36784b5235ed6fa9f4f8
youngsoohan <amazingmobdev@gmail.com>
2021-10-10 18:54:45 +0900

    init react 18

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+const App = () => <div>Hello, Webpack!</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        // 1
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        // 2
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3011,
+    open: true,
+  },
+};
